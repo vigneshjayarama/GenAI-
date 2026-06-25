b8e6143a5843b0631379312e4ce4067dc932aaa9
vigneshjayarama <viky.vignesh99@gmail.com>
2026-06-25 16:50:48 +0530

    Update API key and clean up comments in test file
    Updated API key initialization and removed commented code.

---
+++ GenAI_Chat_test.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+from google import genai
+from google.genai import types
+st.markdown("<h1 style='color: black; font-size: 40px;'>PYTHON AI ASSISTANT</h1><p style='color: blue; font-size: 20px;'>Python AI Assistant can help to your questions related to python</p>", unsafe_allow_html=True)
+
+robo = genai.Client(api_key='API_KEY')
+
+
+mychat = robo.chats.create(model="gemini-flash-lite-latest")
+response_placeholder = st.empty()
+
+prompt = st.text_input("", placeholder= "Enter your Prompt here....")
+
+col1, col2, col3 = st.columns([4,2,4])
+
+with col2:
+    send = st.button("Send")
+
+if send:
+    # st.write(f"User has sent the following prompt: {prompt}")
+    response = mychat.send_message(prompt)
+    response_placeholder.write(response.text)
